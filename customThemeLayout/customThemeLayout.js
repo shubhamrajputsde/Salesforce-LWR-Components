765f3525493df101201ca4e06f3cbd5817e3c3b1
Shubham Rajput <shubham.rajput0369@gmail.com>
2025-03-02 23:59:02 +0530

    Add files via upload
    Committing customThemeLayout LWR

---
+++ customThemeLayout/customThemeLayout.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot header This is the header component
+ * @slot main This is the main component
+ * @slot footer This is the footer component
+ */
+export default class CustomThemeLayout extends LightningElement {}
